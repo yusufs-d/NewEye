b09c10e555b43bcf82782aa5ae77ad6faf87128e
yusufs-d <yusufsalih.2363.demir@gmail.com>
2024-03-15 01:18:53 +0300

    Update for training

--- detect.py
+++ detect.py
@@ -215,12 +215,16 @@ while True:
             label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
             cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
             cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+            print(f"Object:{label}\n Coordinates: {(xmin, label_ymin-labelSize[1]-10)}, {(xmin+labelSize[0], label_ymin+baseLine-10)}")
 
     # Draw framerate in corner of frame
     cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
     # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object detector', frame)
+    wi, he = cv2.getWindowImageRect("Object detector")
+    print(f"Pencere Boyutu - Genişlik: {wi}, Yükseklik: {he}")
+
 
     # Calculate framerate
     t2 = cv2.getTickCount()
